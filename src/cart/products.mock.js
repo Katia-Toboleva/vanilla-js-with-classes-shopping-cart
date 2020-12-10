2e21243daaa1204754c8489974f67f34433ce4d1
Katia-2020 <kateflights@gmail.com>
2020-12-10 23:34:59 +0000

    products mock file

---
+++ src/cart/products.mock.js
@@ -0,0 +1,86 @@
+export const products = [
+  {
+    id: 0,
+    name: "carrots",
+    description: "Delicious carrots from the greenest fields",
+    image: "https://images.unsplash.com/photo-1606355601253-61a57fe375e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+    price: 1.35
+  },
+  {
+    id: 1,
+    name: "bread",
+    description: "Organic bread baked in wooden oven",
+    image: "https://images.unsplash.com/photo-1551888761-382cd30b5da6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+    price: 0.75
+  },
+  {
+    id: 2,
+    name: "oil",
+    description: "Natural hand picked olive oil",
+    image: "https://media.eataly.net/media/catalog/product/cache/8/image/9df78eab33525d08d6e5fb8d27136e95/C/u/Cufrol-Olio-Evo-Biologico-0.5l-55994.jpg",
+    price: 3.75
+  },
+  {
+    id: 3,
+    name: "pasta",
+    description: "Real Italian pasta",
+    image: "https://images.unsplash.com/photo-1518675219903-c682c4b16b1d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+    price: 2.25
+  },
+  {
+    id: 4,
+    name: "rice",
+    description: "Coming from the fields of Vietnam",
+    image: "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/how-to-cook-rice.jpg",
+    price: 0.95
+  },
+  {
+    id: 5,
+    name: "eggs",
+    description: "Full of protein",
+    image: "https://images.unsplash.com/photo-1559738928-5d5dc5c2458d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+    price: 2.50
+  },
+  {
+    id: 6,
+    name: "potatos",
+    description: "The best choice from the fields",
+    image: "https://freshpointlocal.co.uk/wp-content/uploads/2018/12/Potatoes-2.jpg",
+    price: 3.45
+  },
+  {
+    id: 7,
+    name: "strawberries",
+    description: "Kissed by the sun",
+    image: "https://images.unsplash.com/photo-1587474708610-0cf2d41b59e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=518&q=80",
+    price: 5.50
+  },
+  {
+    id: 8,
+    name: "wine",
+    description: "Direct from the vineyard",
+    image: "https://images.unsplash.com/photo-1580316521041-29524fd519f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+    price: 10.75
+  },
+  {
+    id: 9,
+    name: "chocolate",
+    description: "Dark organic chololate",
+    image: "https://images.unsplash.com/photo-1572302792185-56f963ebbc4c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+    price: 1.75
+  },
+  {
+    id: 10,
+    name: "milk",
+    description: "Milk full of vitamins",
+    image: "https://images.milkandmore.co.uk/image/upload/w_iw/f_auto/w_392,d_back_up_image.jpg,c_scale/v1/products/2004522_1.jpg",
+    price: 0.85
+  },
+  {
+    id: 11,
+    name: "juice",
+    description: "Hand squeezed juice",
+    image: "https://images.unsplash.com/photo-1580296338961-b18339c983fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=488&q=80",
+    price: 2.55
+  }
+];
